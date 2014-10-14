2da59aa07c9792e9b1c1ccab4d06a25821ec8c45
darthvader45 <alienkiller45@gmail.com>
2014-10-14 12:36:53 -0400

    Mud fluid is added.  Bucket is still being coded.

--- src/main/java/com/caske2000/carnivores/blocks/ModBlocks.java
+++ src/main/java/com/caske2000/carnivores/blocks/ModBlocks.java
@@ -2,7 +2,13 @@ package com.caske2000.carnivores.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidContainerRegistry;
+import net.minecraftforge.fluids.FluidRegistry;
 
+import com.caske2000.carnivores.items.ModItems;
 import com.caske2000.carnivores.reference.Reference;
 import com.caske2000.carnivores.tileentities.TileEntityUnknownEgg;
 import com.caske2000.carnivores.tileentities.TileEntityUnknownEggOpen;
@@ -14,16 +20,21 @@ public class ModBlocks
 	public static Block unknownBox;
 	public static Block unknownEgg;
 	public static Block unknownEggOpen;
+	public static Fluid mudFluid;
+	public static Block mudFluidBlock;
 
 public static void init(){
 	unknownBox = new UnknownBox();
 	unknownEgg = new BlockUnknownEgg(Material.wood).setBlockName("UnknownEgg").setBlockTextureName(Reference.MODID + ":" + "unknownEggParticles");
 	unknownEggOpen = new BlockUnknownEggOpen(Material.wood).setBlockName("UnknownEggOpen").setBlockTextureName(Reference.MODID + ":" + "unknownEggParticles");
+	mudFluid = new Fluid("mud").setUnlocalizedName("mud").setLuminosity(0).setDensity(1000).setTemperature(295).setViscosity(3500).setGaseous(false);
 	GameRegistry.registerBlock(unknownEgg, "unknownEgg");
 	GameRegistry.registerBlock(unknownEggOpen, "unknownEggOpen");
 	GameRegistry.registerTileEntity(TileEntityUnknownEgg.class, "");
 	GameRegistry.registerTileEntity(TileEntityUnknownEggOpen.class, "2");
+	FluidRegistry.registerFluid(mudFluid);
+	mudFluidBlock = new BlockMud(mudFluid, Material.water).setBlockName("mud");
+	GameRegistry.registerBlock(mudFluidBlock, "mud");
 	
-
 }
 }
